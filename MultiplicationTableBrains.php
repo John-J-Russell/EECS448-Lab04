1ebbade7f4ed7485d4714b782a7a9636e0a7b5bd
John Russell <j362r647@ku.edu>
2016-02-26 16:11:09 -0600

    First prototype complete

--- MultiplicationTableBrains.php
+++ MultiplicationTableBrains.php
@@ -3,7 +3,17 @@
 	error_reporting(E_ALL);
 	ini_set("display_errors", 1);
 
-
+	//Nested for loop, probably.
+	echo "<table>";
+	for($y=1; $y <=100; $y= $y+1)
+	{
+		echo "<tr>";
+		for($x=1; $x <= 100; $x= $x+1)
+		{
+			echo "<td>"x*y"</td>";
+		}
+		echo "</tr>";
+	}
 
 
 
