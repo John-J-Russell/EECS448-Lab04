258af99cbe7f63ba1e43e7914d01364de23f02f2
John Russell <j362r647@ku.edu>
2016-03-03 15:43:33 -0600

    Add answer output, and a congratulations if user gets 100%

--- QuizFolder/Quiz.php
+++ QuizFolder/Quiz.php
@@ -28,6 +28,32 @@
 	{
 		$numCorrect++;
 	}
-	echo $numCorrect;
+	//echo $numCorrect;
+	echo "Question 1: PV=nR\"_\", what is \"_\"? <br>";
+	echo "You said: " . $q1 . "<br>";
+	echo "Correct answer: \"T\" <br> <br> ";
+
+	echo "Question 2: Before being selectively bred by the Dutch, carrots used to be:<br>";
+	echo "You said: " . $q2 . "<br>";
+	echo "Correct answer: \"Purple\" <br> <br> ";
 	
+	echo "Question 3: In Spanish, the word \"ganga\" means: <br>";
+	echo "You said: " . $q3 . "<br>";
+	echo "Correct answer: \"Bargain\" <br> <br> ";
+
+	echo "Question 4: In Spanish, the word \"Pandilla\" means: <br>";
+	echo "You said: " . $q4 . "<br>";
+	echo "Correct answer: \"Gang\" <br> <br> ";
+
+	echo "Question 5: En inglés, qué es la significa de la palabra \"alubias\"? <br>";
+	echo "You said: " . $q5 . "<br>";
+	echo "Correct answer: \"Beans\" <br> <br> ";
+
+	echo "Total score: " . $numCorrect . "/5 <br>";
+	echo "That's " . ($numCorrect/5)*100 . "% <br>";
+
+	if($numCorrect==5)
+	{
+		echo "<br>Congratulations, you aced this test. I hope you're proud.";
+	}
 ?>
