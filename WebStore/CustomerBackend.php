6cdff8501d93745406bf3d65e93679bdb3b2b5ed
John Russell <j362r647@ku.edu>
2016-03-04 08:35:14 -0600

    Populate CustomerBackend with code, do some styling in CSS for the backend

---
+++ WebStore/CustomerBackend.php
@@ -0,0 +1,24 @@
+<?php
+
+	$staplerQuantity=$_POST["staplerNum"];
+	$staplesQuantity=$_POST["staplesNum"];
+	$stapleRemoverQuantity=$_POST["stapleRemoverNum"];
+	
+	$shippingOption=$_POST["shipOp"];
+	$email=$_POST["user"];
+	$password=$_POST["pass"];
+	
+	echo "<link href=\"Style.css\" rel=\"stylesheet\" type=\"text/css\"/>";
+	
+	echo "Welcome, " . $email;
+	echo "<br> Your password is: " . $password;
+	
+	echo "<body>";
+	echo "<table id=\"output\"> <caption> Receipt </caption> <tr> <th> Item </th> <th> Quantity </th> <th>Price per Unit</th> <th>Subtotal</th> </tr>";
+	echo "<tr> <td>Mini-Stapler</td> <td>" . $staplerQuantity . "</td> <td> $". 15.00 . "</td> <td>" . $staplerQuantity*15.00 . "</td> </tr>";
+	echo "<tr> <td>Box of Staples</td> <td>" . $staplesQuantity . "</td> <td> $" . 5.00 . "</td> <td>" . $staplesQuantity*5.00 . "</td> </tr>";
+	echo "<tr> <td>Staple Remover</td> <td>" . $stapleRemoverQuantity . "</td> <td> $" . 10.00 . "</td> <td>" . $stapleRemoverQuantity*10.00 . "</td> </tr>";
+	echo "<tr id=\"long\"> Total: $". ($staplerQuantity*15.00 + $staplesQuantity*5.00 + $stapleRemoverQuantity*10.00);
+	echo "</body>";
+	
+?>
