f5a6c52f684176d45ae99379ff2fbadb3459f1cb
John Russell <j362r647@ku.edu>
2016-03-04 16:15:51 -0600

    merge conflicts ahoy

--- WebStore/CustomerBackend.php
+++ WebStore/CustomerBackend.php
@@ -18,7 +18,7 @@
 	echo "<tr> <td>Mini-Stapler</td> <td>" . $staplerQuantity . "</td> <td> $". 15.00 . "</td> <td>" . $staplerQuantity*15.00 . "</td> </tr>";
 	echo "<tr> <td>Box of Staples</td> <td>" . $staplesQuantity . "</td> <td> $" . 5.00 . "</td> <td>" . $staplesQuantity*5.00 . "</td> </tr>";
 	echo "<tr> <td>Staple Remover</td> <td>" . $stapleRemoverQuantity . "</td> <td> $" . 10.00 . "</td> <td>" . $stapleRemoverQuantity*10.00 . "</td> </tr>";
-	echo "<tr id=\"long\" rowspan=\"4\"> Total: $". ($staplerQuantity*15.00 + $staplesQuantity*5.00 + $stapleRemoverQuantity*10.00);
+	echo "<tr rowspan=\"4\"> Total: $". ($staplerQuantity*15.00 + $staplesQuantity*5.00 + $stapleRemoverQuantity*10.00);
 	echo "</table>";
 	echo "</body>";
 	
