54694615422c49a9ccc6b2fec1577e9290dacc83
John Russell <j362r647@ku.edu>
2016-03-04 17:51:45 -0600

    I have ascended. I see all. I know all. \n tl;dr I finished it, what a bitch to code, I never want to see this project again. I'm out.

--- WebStore/FormChecker.js
+++ WebStore/FormChecker.js
@@ -1,21 +1,24 @@
 function checkForm()
 {
+	//I'm leaving all these old alerts for posterity. This code was the epitome of a bitch to write.
+	//alert("Reading this at all?");
 	//Validate information
-	alert("triggered");
+	//alert("triggered");
 	//return false;
 	var p1=document.getElementById("staplerOrderNum").value;
-	alert("first order");
+	//alert("first order is" + p1);
 	var p2=document.getElementById("staplesOrderNum").value;
-	alert("second order);
+	//alert("second order is" + p2);
 	var p3=document.getElementById("stapleRemoverOrderNum").value;
-	alert("3rd order");
-	var ship=document.getElementByName("shipOp");
-	alert("shipping option first read");
+	//alert("3rd order is " + p3);
+	var ship=document.getElementsByName("shipOp");
+	//alert("ship option value is " + ship);
+	//alert("shipping option first read is" + ship.length);
 	var shipPrice=null;
-	alert("test");
+	//alert("test shipPrice is" +shipPrice);
 	for(var x=0; x<ship.length; x++)
 	{
-		if(ship[i].checked)
+		if(ship[x].checked)
 		{
 			if(x==0)
 			{
@@ -31,23 +34,24 @@ function checkForm()
 			}
 		}
 	}
-	alert("Shipping option gamut complete");
+	//alert("Shipping option gamut complete, marcked price" + shipPrice);
 	var user=document.getElementById("username").value;
 	var pass=document.getElementById("password").value;
-	alert(validateEmail(user));
-	return false;
+	//alert(validateEmail(user));
+	//return false;
 	if(p1>=0 && p2>=0 && p3>=0 && (shipPrice==0 || shipPrice==5 || shipPrice==50) && (validateEmail(user)) && pass!="")
 	{
-		alert("Working");
-		return(true);
+		//alert("Working");
+		return true;
 		
 	}
 	else
 	{
 		
-		alert("Invalid input");
-		return(false);
+		alert("Invalid input, please try again.");
+		return false;
 	}
+
 }
 
 
@@ -72,7 +76,7 @@ function reset()
 //link to page: http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
 function validateEmail(email) 
 {
-    var re = /\S+@\S+\.\S+/; //I have absolutely no idea what this means.
-	alert(re.test(email));
+    var re = /\S+@\S+\.\S+/; //I have absolutely no idea what this means, beyond being S-Expressions
+
     return re.test(email);
 }
