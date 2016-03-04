6f00fb4af42c2fb0da659223e7dbb1a0fd149dfb
John Russell <j362r647@ku.edu>
2016-03-04 13:16:00 -0600

    Add shipping option price to receipt

--- WebStore/CustomerBackend.php
+++ WebStore/CustomerBackend.php
@@ -8,6 +8,20 @@
 	$email=$_POST["user"];
 	$password=$_POST["pass"];
 	
+	$shippingOptionText="a";
+	if($shippingOption==0)
+	{
+		$shippingOptionText="Free 7-day";
+	}
+	else if ($shippingOptionText==5)
+	{
+		$shippingOptionText="$5.00 3-day";
+	}
+	else
+	{
+		$shippingOptionText="$50.00 Overnight";
+	}
+	
 	echo "<link href=\"StoreStyle.css\" rel=\"stylesheet\" type=\"text/css\"/>";
 	
 	echo "Welcome, " . $email;
@@ -18,7 +32,8 @@
 	echo "<tr> <td>Mini-Stapler</td> <td>" . $staplerQuantity . "</td> <td> $". 15.00 . "</td> <td>" . $staplerQuantity*15.00 . "</td> </tr>";
 	echo "<tr> <td>Box of Staples</td> <td>" . $staplesQuantity . "</td> <td> $" . 5.00 . "</td> <td>" . $staplesQuantity*5.00 . "</td> </tr>";
 	echo "<tr> <td>Staple Remover</td> <td>" . $stapleRemoverQuantity . "</td> <td> $" . 10.00 . "</td> <td>" . $stapleRemoverQuantity*10.00 . "</td> </tr>";
-	echo "<tr id=\"long\" rowspan=\"4\"> Total: $". ($staplerQuantity*15.00 + $staplesQuantity*5.00 + $stapleRemoverQuantity*10.00);
+	echo "<tr> <td rowspan=\"2\"> Shipping Option: </td> <td> " . $shippingOptionText . "</td> <td> $" . $shippingOption . ".00 </td> </tr>";
+	echo "<tr> <td> id=\"long\" rowspan=\"4\"> Total: $". ($staplerQuantity*15.00 + $staplesQuantity*5.00 + $stapleRemoverQuantity*10.00) . "</td> </tr>";
 	echo "</table>";
 	echo "</body>";
 	
